343d665026f9a8c5c8a5116653740df0c196f1fa
kristina.gagalovska <kristinagagalovska@gmail.com>
2016-05-30 22:56:59 +0200

    Fix users table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('number');
             $table->string('email')->unique();
             $table->boolean('isadmin');
             $table->string('password');
