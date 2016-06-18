5963b3cfee2f073b0fc472f0d72d5b370a78cb9b
kristina.gagalovska <kristinagagalovska@gmail.com>
2016-06-18 22:40:43 +0200

    fixes #2

--- resources/views/admin/advertisements.blade.php
+++ resources/views/admin/advertisements.blade.php
@@ -2,27 +2,43 @@
 
 @section('content')
     <div class="container">
-                <div class="panel panel-default">
-                    <div class="panel-body">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-8 col-md-offset-2">
-                                    <div class="panel panel-default">
-                                        <div class="panel-body">
-                                            @foreach($advertisements as $advertisement)
-                                                <div class="list-group">
-                                                    <a href="{{ route('advertisement.view', $advertisement->id) }}" class="list-group-item">
-                                                        <h1 class="list-group-item-heading">{{ $advertisement->title }}</h1>
-                                                        <p class="list-group-item-text">{{ $advertisement->description}}</p>
-                                                    </a>
-                                                </div>
-                                            @endforeach
+        <div class="panel panel-default">
+            <div class="panel-body">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-md-8 col-md-offset-2">
+                            <div class="panel panel-default">
+                                <div class="panel-body">
+                                    @foreach($advertisements as $advertisement)
+                                        <div>
+                                            <a href="{{ route('advertisement.view', $advertisement->id) }}" class="list-group-item">
+                                                <h1 class="list-group-item-heading">{{ $advertisement->title }}</h1>
+                                                <p class="list-group-item-text">{{ $advertisement->description}}</p>
+                                            </a>
                                         </div>
-                                    </div>
+                                        <div class="row">
+                                            <div class="col-md-4 col-md-offset-4">
+                                                <form method="GET" action="{{ route('advertisement.edit', $advertisement->id) }}">
+                                                    <input type="hidden" name="_method" value="EDIT" />
+                                                    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                                                    <button type="submit" class="btn btn-danger" >Измени оглас</button>
+                                                </form>
+                                            </div>
+                                            <div class="col-md-4 col-md-8">
+                                                <form method="POST" action="{{ route('advertisement.delete', $advertisement->id) }}">
+                                                    <input type="hidden" name="_method" value="DELETE" />
+                                                    <input type="hidden" name="_token" value="{{ csrf_token() }}" />
+                                                    <button type="submit" class="btn btn-default">Избриши оглас</button>
+                                                </form>
+                                            </div>
+                                        </div></br>
+                                    @endforeach
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
     </div>
 @endsection
--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -6,7 +6,7 @@
             <div class="col-md-10 col-md-offset-1">
                 <div class="panel panel-default">
                     <div class="panel-body">
-                        <h3><a href="{{ route('admin.advertisements') }}">Menage advertisements</a></h3>
+                        <h3><a href="{{ route('admin.advertisements') }}">Преглед на сите огласи</a></h3>
                     </div>
                 </div>
             </div>
